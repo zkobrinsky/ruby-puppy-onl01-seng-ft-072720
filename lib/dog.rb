a327eae854f0cff590d1f190e472bd2b3d79dc3f
Zach Kobrinsky <zachkobrinsky@gmail.com>
2020-08-06 19:07:37 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,38 @@
-# Add your code here
+require "pry"
+
+class Dog
+
+  @@all = []
+  @@name = []
+
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+
+    if !@@name.include? (name)
+    @@name << name
+    end
+
+    self.save
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.clear_all
+    @@all.clear
+    @@name.clear
+  end
+
+  def self.print_all
+    puts @@name.join("\n")
+  end
+
+end
